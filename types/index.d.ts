9dfee7e8621d19f88381734b3332c9e45fadeb47
Keisuke Kan <9renpoto@gmail.com>
2019-03-10 15:44:48 +0900

    feat(types) Added types and tests

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+import React from "react"
+import { History } from 'history'
+
+export interface Props {
+  id: string, // Google Analytics Tracking ID
+  basename?: string,
+  debug?: boolean,
+  trackPathnameOnly?: boolean,
+  children?: React.ReactNode,
+}
+
+export default class ReactRouterGA extends React.Component<Props> {
+  render(): JSX.Element
+}
