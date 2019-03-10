83af40f40a0c895c95382508257e7d3148fdc716
Keisuke Kan <9renpoto@gmail.com>
2019-03-10 15:51:36 +0900

    format with prettier

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,16 +2,16 @@ import { create } from "react-test-renderer";
 import React, { Component } from "react";
 import Analytics from "../";
 import { MemoryRouter } from "react-router";
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
 describe("ReactRouterGA", () => {
   beforeAll(() => {
-    document.body.innerHTML = `<script src='//hello.js' />`
-  })
+    document.body.innerHTML = `<script src='//hello.js' />`;
+  });
 
   class Page extends Component {
-    render () {
-      return (<div>Hello</div>)
+    render() {
+      return <div>Hello</div>;
     }
   }
 
