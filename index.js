b96023dda0c3d726b4c6e10064199a1faa0cf24c
Federico Knüssel <fknussel@gmail.com>
2020-03-05 21:18:27 +1100

    Added window check on init (#15)
    * Added window check on init
    
    * Added window fix also in source file

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ class ReactRouterGA extends React.Component<Props> {
       return;
     }
 
+    // Check if window exists for static compiling
+    if (typeof window === "undefined") {
+      return;
+    }
+
     // Load Google Analytics
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
